19eab13a9910618116588c106c75f1fe59227ba0
Xutianye <314565807@qq.com>
2026-05-29 17:14:29 +0800

    init: project baseline
    Add application config, project structure and settings

---
+++ src/main/java/org/xutianye/intelligentchunkstudio/IntelligentChunkStudioApplication.java
@@ -0,0 +1,13 @@
+package org.xutianye.intelligentchunkstudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelligentChunkStudioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IntelligentChunkStudioApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/xutianye/intelligentchunkstudio/IntelligentChunkStudioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.xutianye.intelligentchunkstudio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelligentChunkStudioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
